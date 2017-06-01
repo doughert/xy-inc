1d9eeb36c527428dc762148f6de7039a5ab19885
Doughert Vasconcelos Fernandes <doughertvf@gmail.com>
2017-05-31 22:47:45 -0300

    test

--- src/main/java/com/xyinc/poi/model/Poi.java
+++ src/main/java/com/xyinc/poi/model/Poi.java
@@ -23,6 +23,15 @@ public class Poi {
 	@Column(name = "name")
 	private String name;
 
+	public Poi() {
+	}
+
+	public Poi(Integer coordinateX, Integer coordinateY, String name) {
+		this.coordinateX = coordinateX;
+		this.coordinateY = coordinateY;
+		this.name = name;
+	}
+
 	public Long getId() {
 		return id;
 	}
@@ -53,6 +62,31 @@ public class Poi {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Poi other = (Poi) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
 	}
 
 	@Override
--- src/test/java/com/xyinc/poi/PoiApplicationTests.java
+++ src/test/java/com/xyinc/poi/PoiApplicationTests.java
@@ -1,16 +1,51 @@
 package com.xyinc.poi;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.xyinc.poi.model.Poi;
+import com.xyinc.poi.service.PoiService;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class PoiApplicationTests {
+
+	@Autowired
+	private PoiService service;
 
 	@Test
 	public void contextLoads() {
+
+		Poi lanchonete = new Poi(27, 12, "Lanchonete");
+		service.save(lanchonete);
+
+		Poi posto = new Poi(31, 18, "Posto");
+		service.save(posto);
+
+		Poi joalheria = new Poi(15, 12, "Joalheria");
+		service.save(joalheria);
+
+		Poi floricultura = new Poi(19, 21, "Floricultura");
+		service.save(floricultura);
+
+		Poi pub = new Poi(12, 8, "Pub");
+		service.save(pub);
+
+		Poi supermercado = new Poi(23, 6, "Supermercado");
+		service.save(supermercado);
+
+		Poi churrascaria = new Poi(28, 2, "Churrascaria");
+		service.save(churrascaria);
+
+		assertThat(service.listByProximity(20, 20, 10)).isNotEmpty();
+
 	}
 
 }
